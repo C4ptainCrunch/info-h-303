4c97a13c35eb2e073749d32ff1423c242d5e2894
Nikita Marchant <nikita.marchant@gmail.com>
2016-04-28 13:45:14 +0200

    no types

--- source/createdb.sql
+++ source/createdb.sql
@@ -1,18 +1,3 @@
--- Types
-------------------------------------
-
-CREATE TYPE coordinate AS (
-  "latitude" NUMERIC(9, 6),
-  "longitude" NUMERIC(9, 6)
-);
-
-CREATE TYPE address AS (
-  "street" VARCHAR(254),
-  "number" INTEGER, -- CHECK ("number" > 0),
-  "zip" INTEGER, -- CHECK ("zip" > 0),
-  "city" VARCHAR(254)
-);
-
 CREATE TYPE etablissement_type AS ENUM ('restaurant', 'hotel', 'bar');
 
 -- Tables
@@ -37,22 +22,29 @@ CREATE TABLE "etablissement" (
   "name" VARCHAR(254) NOT NULL,
   "phone" VARCHAR(20) NOT NULL,
   "url" TEXT,
-  "address" address NOT NULL ,
-  "gps" coordinate NOT NULL,
+  --- address
+    "address_street" VARCHAR(254),
+    "address_number" INTEGER CHECK ("address_number" > 0),
+    "address_zip" INTEGER CHECK ("address_zip" > 0),
+    "address_city" VARCHAR(254),
+  --- gps
+    "latitude" NUMERIC(9, 6) CHECK("latitude" BETWEEN -180 AND 180),
+    "longitude" NUMERIC(9, 6) CHECK("longitude" BETWEEN -180 AND 180),
   "created" DATE NOT NULL, -- creation > user.creation
-  "user_id" INTEGER NOT NULL REFERENCES "user",
+  "user_id" INTEGER NOT NULL REFERENCES "user" ON DELETE RESTRICT,
   "type" etablissement_type NOT NULL
+  "picture" VARCHAR(100),
 );
 
 CREATE TABLE "hotel" (
-  "etablissement_id" INTEGER PRIMARY KEY REFERENCES "etablissement",
-  "stars" INTEGER NOT NULL CHECK ("stars" > 0 AND "stars" < 6),
+  "etablissement_id" INTEGER PRIMARY KEY REFERENCES "etablissement" ON DELETE CASCADE,
+  "stars" INTEGER NOT NULL CHECK ("stars" BETWEEN 0 AND 5),
   "rooms" INTEGER NOT NULL CHECK ("rooms" > 0),
   "price" INTEGER NOT NULL CHECK ("price" > 0)
 );
 
 CREATE TABLE "restaurant" (
-  "etablissement_id" INTEGER PRIMARY KEY REFERENCES "etablissement",
+  "etablissement_id" INTEGER PRIMARY KEY REFERENCES "etablissement" ON DELETE CASCADE,
   "price_range" INTEGER NOT NULL CHECK ("price_range" > 0),
   "max_seats" INTEGER NOT NULL CHECK ("max_seats" > 0),
   "takeaway" BOOLEAN NOT NULL,
@@ -61,17 +53,17 @@ CREATE TABLE "restaurant" (
 );
 
 CREATE TABLE "bar" (
-  "etablissement_id" INTEGER PRIMARY KEY REFERENCES "etablissement",
+  "etablissement_id" INTEGER PRIMARY KEY REFERENCES "etablissement" ON DELETE CASCADE,
   "smoker" BOOLEAN NOT NULL,
   "food" BOOLEAN NOT NULL
 );
 
 CREATE TABLE "comment" (
   "id" SERIAL PRIMARY KEY,
-  "user_id" INTEGER NOT NULL REFERENCES "user",
-  "etablissement_id" INTEGER NOT NULL REFERENCES "etablissement",
+  "user_id" INTEGER NOT NULL REFERENCES "user" ON DELETE CASCADE,
+  "etablissement_id" INTEGER NOT NULL REFERENCES "etablissement" ON DELETE CASCADE,
   "date" DATE NOT NULL, -- check > etablissement.date AND > user.date
-  "score" INTEGER NOT NULL CHECK ("score" > 0 AND "score" < 6),
+  "score" INTEGER NOT NULL CHECK ("score" BETWEEN 0 AND 5),
   "text" TEXT NOT NULL,
   UNIQUE ("date", "user_id")
 );
@@ -79,9 +71,9 @@ CREATE TABLE "comment" (
 
 CREATE TABLE "etablissement_label" (
   "id" SERIAL PRIMARY KEY,
-  "etablissement_id" INTEGER NOT NULL REFERENCES "etablissement",
-  "user_id" INTEGER NOT NULL REFERENCES "user",
-  "label_id" INTEGER NOT NULL REFERENCES "label",
+  "etablissement_id" INTEGER NOT NULL REFERENCES "etablissement" ON DELETE CASCADE,
+  "user_id" INTEGER NOT NULL REFERENCES "user" ON DELETE CASCADE,
+  "label_id" INTEGER NOT NULL REFERENCES "label" ON DELETE CASCADE,
   UNIQUE ("etablissement_id", "user_id", "label_id")
 );
 
