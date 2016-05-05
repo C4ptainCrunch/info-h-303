12c66677f78bf3d4d05d3306cf52adf56c457414
Romain Fontaine <romain-fontaine@outlook.com>
2016-05-05 03:19:05 +0200

    try the 6th yolo request https://www.youtube.com/watch?v=LB-RUwDaz20

--- source/requetes.sql
+++ source/requetes.sql
@@ -6,3 +6,10 @@ select * from etablissement where id in (select etablissement_id from comment GR
 
 -- R5:
 select etablissement.* from etablissement join comment on etablissement.id = comment.etablissement_id GROUP BY etablissement.id HAVING COUNT(*) >=3 ORDER BY avg(score);
+
+-- R6:
+select label.name from label inner join (select etablissement_label.label_id, etablissement.id, avg(comment.score) as score from etablissement_label join etablissement on etablissement_label.etablissement_id = etablissement.id join comment on etablissement.id = comment.etablissement_id GROUP BY etablissement.id, etablissement_label.label_id) e on label.id = e.label_id GROUP BY label.id HAVING count(*) > 5 order by avg(e.score);
+-- Inorrecte, à fixer car devrais renvoyer le même nombre de ligne que:
+select label.name from label join etablissement_label on label.id = etablissement_label.label_id GROUP BY label.id HAVING count(*) > 5;
+-- Should be working:
+select label.* from label full join (select etablissement_label.label_id as id, avg(comment.score) as score from etablissement_label left join etablissement on etablissement_label.etablissement_id = etablissement.id full join comment on etablissement.id = comment.etablissement_id group by etablissement_label.label_id, etablissement.id, etablissement_label.user_id) l on label.id = l.id group by label.id HAVING count(*) >= 5 order by avg(l.score);
