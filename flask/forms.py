7d60eec744b26e89a2e5608ccda195ba233a46c1
Nikita Marchant <nikita.marchant@gmail.com>
2016-05-09 11:53:25 +0200

    register

--- flask/forms.py
+++ flask/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import Form
-from flask_wtf.html5 import TelField, URLField, IntegerField, DecimalField
+from flask_wtf.html5 import TelField, URLField, IntegerField, DecimalField, EmailField
 from flask_wtf.file import FileField
 from wtforms import TextField, TextAreaField, SubmitField, validators, PasswordField
 
@@ -30,5 +30,16 @@ class Hotel(Etablissement):
 class Login(Form):
     username = TextField("Nom d'utilisateur")
     password = PasswordField('Mot de passe')
+
+    submit = SubmitField("Envoyer")
+
+class User(Form):
+    username = TextField("Nom d'utilisateur")
+    email = EmailField("Adresse mail")
+    password = PasswordField('Mot de passe', [
+        validators.Required(),
+        validators.EqualTo('confirm', message='Les mots de passe doivent être identiques')
+    ])
+    confirm = PasswordField('Mot de passe (à nouveau)')
 
     submit = SubmitField("Envoyer")
