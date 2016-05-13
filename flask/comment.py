df64b2231d2f1ea0aa15535672511754f3f9745b
Romain Fontaine <romain-fontaine@outlook.com>
2016-05-13 19:57:42 +0200

    oups

---
+++ flask/comment.py
@@ -0,0 +1,32 @@
+from flask import Blueprint, render_template, request, g, redirect, url_for, session, abort, flash, jsonify, json, Response
+from functools import wraps
+import psycopg2
+import psycopg2.extras
+from flask_bootstrap import Bootstrap
+import forms
+import config
+import models
+from datetime import datetime
+import statistics
+import etablissement
+
+from ressources import *
+
+comment_api = Blueprint('comment_api', __name__)
+
+@comment_api.route("/add/<int:epk>/<int:uid>", methods=["POST"])
+@admin_required
+def add_comment(epk, uid):
+    form = forms.Comment(request.form)
+    if request.method == 'POST' and form.validate():
+        comment = models.Comment()
+        form.populate_obj(comment)
+
+        comment.user_id = uid
+        comment.etablissement_id = epk
+        comment.date = datetime.now().date()
+        comment.insert(g.cursor)
+
+        return redirect(url_for('get_etablissement', pk=epk))
+
+    return render_template('add_hotel.html', form=form)
