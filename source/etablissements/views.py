c71fa0d0a03d09ac7ea9efdcc2eed6d06b6cea93
Nikita Marchant <nikita.marchant@gmail.com>
2016-05-13 22:16:51 +0200

    Remove source/

--- source/etablissements/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from etablissements.models import Etablissement
-
-def list_etablissements(request):
-    context = {"bars" : [Etablissement(name="Bastoche"), Etablissement(name="Tavernier")]}
-    return render(request, "etablissements_list.html", context)
